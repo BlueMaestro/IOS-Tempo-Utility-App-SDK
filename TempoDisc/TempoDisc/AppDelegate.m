45e2c18a9114cb26c3af842d63c61b7a865f664d
Nikola Misic <nikola.misic@htecgroup.com>
2017-03-09 20:58:40 +0100

    deleting database on schema mismatch

--- TempoDisc/TempoDisc/AppDelegate.m
+++ TempoDisc/TempoDisc/AppDelegate.m
@@ -99,7 +99,10 @@
         // Replace this with code to handle the error appropriately.
         // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
         NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
-        abort();
+//        abort();
+		[[NSFileManager defaultManager] removeItemAtURL:storeURL error:nil];
+		_persistentStoreCoordinator = nil;
+		return [self persistentStoreCoordinator];
     }
     
     return _persistentStoreCoordinator;
